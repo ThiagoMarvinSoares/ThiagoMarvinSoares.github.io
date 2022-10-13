73a5fc27973c611cfa20213f84bbc51f96d0d08f
Thiago Marvin <thiagomarvin.soares@gmail.com>
2022-10-13 19:45:34 -0300

    roll dice commit and some starting configs

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -4,6 +4,8 @@ const player0Score = document.querySelector('#score--0');
 const player1Score = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,7 +14,7 @@ const btnHold = document.querySelector('.btn--hold');
 
 
 //starting condition
-let scores = [0, 1];
+let scores = [0, 0];
 player0Score.innerHTML = 0;
 player1Score.innerHTML = 0;
 diceEl.classList.add('hidden');
@@ -32,6 +34,10 @@ btnRoll.addEventListener('click', function(){
         currentScore += roll;
         document.querySelector(`#current--${activePlayer}`).innerHTML = currentScore;
     } else {
+        document.querySelector(`#current--${activePlayer}`).innerHTML = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 });
