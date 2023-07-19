b1cf267269997a281b83c79886c1f956077a88ad
Thiago <thiagomarvin.soares@gmail.com>
2023-07-19 08:48:21 -0300

    Project Done

--- dnc-react-js-challenge-main/src/components/Header.jsx
+++ dnc-react-js-challenge-main/src/components/Header.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import "./Index.scss"
+import React from "react";
+import "./Index.scss";
 
 const Header = () => {
   return (
-    <header className='header'>
-        <div className='header__items--wrap'>
-            <h2 className='header__title'>Organizacão</h2>
-            <h2 className='header__title selected'>Tarefas</h2>
-        </div>
+    <header className="header">
+      <div className="header__items--wrap">
+        <h2 className="header__title">Organizacão</h2>
+        <h2 className="header__title selected">Tarefas</h2>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- dnc-react-js-challenge-main/src/components/deleteModal/modal.jsx
+++ dnc-react-js-challenge-main/src/components/deleteModal/modal.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./index.scss";
 
-const modal = ({open, onClose, onDelete }) => {
-  if (!open) return null
+const modal = ({ open, onClose, onDelete, taskText }) => {
+  if (!open) return null;
   return (
-    <div className='modalWindow overlay'>
-      <div className='modalWindow--wrap'>
-        <h2 className='modalWindow__title'>Deseja excluir esse item?</h2>
-          <div className='modalWindow__buttons'>
-            <button className='modalWindow__buttons--button' onClick={onDelete}>Sim</button>
-            <button className='modalWindow__buttons--button' onClick={onClose}>Não</button>
-          </div>
+    <div className="modalWindow overlay">
+      <div className="modalWindow--wrap">
+        <h2 className="modalWindow__title">Deseja excluir esse item?</h2>
+        <p className="modalWindow__taskText">{taskText}</p>
+        <div className="modalWindow__buttons">
+          <button className="modalWindow__buttons--button" onClick={onDelete}>
+            Sim
+          </button>
+          <button className="modalWindow__buttons--button" onClick={onClose}>
+            Não
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default modal
+export default modal;
